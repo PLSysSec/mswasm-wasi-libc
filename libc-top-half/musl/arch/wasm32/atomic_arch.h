3efdcb6fea5ac930f9cae66e716e532c3a8d54c3
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2021-11-10 11:00:26 -0800

    unsafe definitions of atomics for mswasm just to get things compiling

--- libc-top-half/musl/arch/wasm32/atomic_arch.h
+++ libc-top-half/musl/arch/wasm32/atomic_arch.h
@@ -9,3 +9,21 @@
 #define a_clz_64 __builtin_clzll
 #define a_ctz_32 __builtin_ctz
 #define a_ctz_64 __builtin_ctzll
+
+// added for ms-wasm, thread-unsafe, but ...
+#define a_ll_p a_ll_p
+static inline void *a_ll_p(volatile void *p)
+{
+    return *(void* volatile*)p;
+}
+
+// added for ms-wasm, thread-unsafe, but ...
+#define a_sc_p a_sc_p
+static inline int a_sc_p(volatile int *p, void *v)
+{
+    *(void* volatile*)p = v;
+    return 1;
+}
+
+static inline void a_pre_llsc() {}
+static inline void a_post_llsc() {}
